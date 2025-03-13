772a31d6ef59ddc253122429bdca380d1d619689
mahditalaldev <mahditalal.789@gmail.com>
2025-03-14 01:52:32 +0200

    signup form with the api validation and token in local storage

--- Foto-flo-clinet/src/Components/HomePage.jsx
+++ Foto-flo-clinet/src/Components/HomePage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default HomePage
+  useEffect(() => {
+    const token = localStorage.getItem("user_token");
+    if (!token) {
+      navigate("/signup");
+    }
+  }, [navigate]);
+
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
